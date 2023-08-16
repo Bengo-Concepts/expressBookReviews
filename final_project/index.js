2f6db963d613726b725e8014b7cde828a5116ead
Julien Benghozi <122467799+Bengo-Concepts@users.noreply.github.com>
2023-08-16 15:53:23 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,37 +1,62 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
-
+const session = require('express-session');
+const { isValid, authenticatedUser } = require('./router/auth_users.js');
+const { regd_users } = require('./router/auth_users.js');
+const { public_users } = require('./router/general.js');
+const { isValidISBN } = require('./router/booksdb.js');
 const app = express();
+const PORT = 5000;
 
 app.use(express.json());
 
-app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+app.use(session({
+    secret: "fingerpint",
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-    if(req.session.authorization) {
-        token = req.session.authorization['accessToken'];
-        jwt.verify(token, "access",(err,user)=>{
-            if(!err){
-                req.user = user;
-                next();
-            }
-            else{
-                return res.status(403).json({message: "User not authenticated"})
-            }
-         });
-     } else {
-         return res.status(403).json({message: "User not logged in"})
-     }
+app.post("/customer/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        const accessToken = jwt.sign({ username }, 'secret_key', { expiresIn: '1h' });
+        req.session.authorization = { accessToken, username };
+        return res.status(200).json({ message: "User successfully logged in", accessToken });
+    } else {
+        return res.status(401).json({ message: "Invalid Login. Check username and password" });
+    }
 });
- 
-const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use("/customer/auth/*", (req, res, next) => {
+    if (req.session.authorization && req.session.authorization.accessToken) {
+        const token = req.session.authorization.accessToken;
+        jwt.verify(token, 'secret_key', (err, decoded) => {
+            if (!err) {
+                req.user = decoded.username;
+                next();
+            } else {
+                res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        res.status(403).json({ message: "User not logged in" });
+    }
+});
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.use("/customer", regd_users);
+app.use("/", public_users);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+});
